fd9bd3595115b9a6eefb469a20108899dcdb51d5
Qaizx <kaothom@gmail.com>
2021-03-14 16:14:55 +0700

    debug code finish

--- .github/workflows/2048update.cpp
+++ .github/workflows/2048update.cpp
@@ -5,44 +5,54 @@ using namespace std;
 
 
 
-void command(char command){
-	int a[4][4] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-	if(command == 'w'){
-		up(a);
-		checkover(a);
-		addblock(a);
-		disp(a);
-	}else if(command == 's'){
-		down(a);
-		checkover(a);
-		addblock(a);
-		disp(a);
-	}else if(command == 'a'){
-		left(a);
-		checkover(a);
-		addblock(a);
-		disp(a);
-	}else if(command == 'd'){
-		right(a);
-		checkover(a);
-		addblock(a);
-		disp(a);
-	}	
+void command(int command[4][4]){
+	int ch,tmp[4][4]={0};
+	while (1)
+    {
+    	for(int i=0;i<4;i++)
+    		for(int j=0;j<4;j++)
+    			tmp[i][j]=command[i][j];
+    	ch=getch();
+    	system("cls");
+    	if(ch==72) up(command);
+    	if(ch==80) down(command);
+    	if(ch==75) left(command);
+    	if(ch==77) right(command);
+		if(ch==27) break;
+		
+		if(!check(tmp,command))
+			addblock(command);
+		disp(command);
+			
+		if(!checkover(command))
+		{
+			cout<<"\n\n\t\t\tGAME OVER!!\n\n\n";
+			getch();
+			break;
+		}	
+	}
+
 }
 
 int main(){
-	int a[4][4] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-	char comm;
-	//cout << "Do you want to play 2048???\n";
-	//cout << "Press n to play!!!!!";
-	cin >> comm;
-	if(comm == 'n'){
-		addblock(a);
-		disp(a);
+		cout<<"\n\n\n\n\t\t\t2048 GAME\n\n\n\t\tPress any key to continue";
+	getch();
+	system("cls");
+	int i1,i2,i3,i4,i,j;
+	int a[4][4]={0},tmp[4][4]={0};
+	srand(time(0));
+	i1=rand()%4;
+	i2=rand()%4;
+	while(1)
+	{
+		i3=rand()%4;
+		i4=rand()%4;
+		if(i3!=i1 && i4!=i2) break;
 	}
-	while(true){
-	cin >> comm;
-	command(comm);
-	}
-    return 0;
+	a[i1][i2]=2;
+	a[i3][i4]=4;
+	disp(a);
+	
+	command(a);
+	
 }
